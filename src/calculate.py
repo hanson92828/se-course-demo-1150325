77f773c60edb90eb165991611804c392cb93be9c
hanson92828 <hanson92828@gmail.com>
2026-04-10 09:53:47 +0800

    add sub func with error type

--- src/calculate.py
+++ src/calculate.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-import numpy
 
 app = FastAPI()
 
 def add_func(a: float,b: float) -> float:
     return a+b
+
+def sub_func(a,b):
+    return a-b
 
 # -------------
 
@@ -17,3 +19,9 @@ def calculate_add(a: float, b: float):
     # add_func
     result = add_func(a, b)
     return {"operation": "addition", "a": a, "b": b, "result": result}
+
+@app.get("/sub")
+def calculate_sub(a: float, b: float):
+    # sub_func
+    result = sub_func(a, b)
+    return {"operation": "substraction", "a": a, "b": b, "result": result}
